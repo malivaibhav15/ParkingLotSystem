032286fd13d0c13ceb74d42975a0b3f9df50a27f
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-15 18:39:43 +0530

    ADDED : Test case to check vehicle is parked or not

--- src/test/java/com/BridgeLabz/TestCasesForParkingLotSystem.java
+++ src/test/java/com/BridgeLabz/TestCasesForParkingLotSystem.java
@@ -1,4 +1,14 @@
 package com.BridgeLabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestCasesForParkingLotSystem {
+    @Test
+    public void givenAVehicle_WhenPark_ShouldReturnTrue() {
+        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        boolean isPark = parkingLotSystem.park(new Object());
+        Assert.assertTrue(isPark);
+
+    }
 }
