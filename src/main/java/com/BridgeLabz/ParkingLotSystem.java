0509f00f2e48eda45dd571f7cf3e788f572eb5fa
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-15 17:29:00 +0530

    ADDED : Welcome message

---
+++ src/main/java/com/BridgeLabz/ParkingLotSystem.java
@@ -0,0 +1,9 @@
+package com.BridgeLabz;
+
+public class ParkingLotSystem
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to the parking lot system");
+    }
+
+}
