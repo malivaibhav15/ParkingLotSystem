c5fb5e112624c6ff868eaa4f0a7dd3d3317f4b3a
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-19 14:08:58 +0530

    ADDED : If statement for empty parking lot

--- src/main/java/com/BridgeLabz/ParkingLotSystem.java
+++ src/main/java/com/BridgeLabz/ParkingLotSystem.java
@@ -1,22 +1,33 @@
 package com.BridgeLabz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLotSystem {
+    public int parkingCapacity;
+    public List list = new ArrayList();
     private Object vehicle;
 
-    public ParkingLotSystem() {
+    public ParkingLotSystem(int parkingCapacity) {
+        this.parkingCapacity = parkingCapacity;
     }
 
-    public boolean park(Object vehicle) throws ParkingLotSystemException {
-        if (this.vehicle != null)
-            throw new ParkingLotSystemException("Parking is full");
-        this.vehicle = vehicle;
-        return true;
+    public boolean park(Object vehicle) {
+        if (list.size() == this.parkingCapacity) {
+            Owner.ParkingLotInformation = "Parking is full";
+            AirportSecurity.parkingLotInformation = "Parking is full";
+        } else {
+            list.add(vehicle);
+            return true;
+        }
+        return false;
     }
 
     public boolean unPark(Object vehicle) {
-        if (vehicle == null) return false;
-        if(this.vehicle.equals(vehicle)) {
-            this.vehicle = null;
+        if (list.contains(vehicle)) {
+            list.remove(vehicle);
+            if (list.size() == 0)
+                Owner.ParkingLotInformation = "Parking lot is empty";
             return true;
         }
         return false;
