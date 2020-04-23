fd0fa53e763aaaeccfa3ab702557c6796710364f
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-23 23:49:59 +0530

    ADDED : New exception type

--- src/main/java/com/bridgelabz/exception/ParkingLotSystemException.java
+++ src/main/java/com/bridgelabz/exception/ParkingLotSystemException.java
@@ -1,4 +1,14 @@
 package com.bridgelabz.exception;
 
-public class ParkingLotSystemException {
+public class ParkingLotSystemException extends Exception
+{
+    public enum ExceptionType {
+        PARKING_LOT_FULL, VEHICLE_ALREADY_UNPARKED_OR_WRONG_VEHICLE;
+    }
+    public ExceptionType type;
+
+    public ParkingLotSystemException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
 }
