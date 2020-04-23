e66664375d5010ca6416a1831831e12d84dba5af
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-23 23:59:26 +0530

    REFACTORED : Change in variable name

--- src/main/java/com/bridgelabz/Observer/AirportPersonnel.java
+++ src/main/java/com/bridgelabz/Observer/AirportPersonnel.java
@@ -1,6 +1,19 @@
-package com.bridgelabz;
+package com.bridgelabz.Observer;
 
-public class AirportPersonnel
+import com.bridgelabz.service.ParkingLotSystem;
+
+import java.util.HashMap;
+
+public class AirportPersonnel implements Observer
 {
+    boolean isParkingFull;
 
+    public boolean isParkingFull() {
+        return isParkingFull;
+    }
+
+    @Override
+    public void sendParkingStatus(HashMap<Integer, String> parkingLot) {
+        isParkingFull = (ParkingLotSystem.isParkingLotFull(parkingLot))? true : false;
+    }
 }
