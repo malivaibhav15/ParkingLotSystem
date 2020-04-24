fd5f88a9120ae39924259e8d12c180e8e693cabd
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-24 21:36:30 +0530

    ADDED : Method for finding the details of the perticular car

--- src/main/java/com/bridgelabz/utilities/PoliceDepartment.java
+++ src/main/java/com/bridgelabz/utilities/PoliceDepartment.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 public class PoliceDepartment
 {
     private final ParkingLotSystem parkingLotSystem;
-
+    List colouredVehicle = new ArrayList();
     public PoliceDepartment(ParkingLotSystem parkingLotSystem)
     {
         this.parkingLotSystem = parkingLotSystem;
@@ -27,7 +27,33 @@ public class PoliceDepartment
                     .filter(map -> colour.equals(map.getValue().colour))
                     .map(Map.Entry::getKey)
                     .collect(Collectors.toList());
+            this.colouredVehicle.addAll(colouredVehicle);
         }
         return colouredVehicle;
+    }
+    public List getDetailsOfParticularTypeOfVehicle(String carCompany, String colour) {
+        List<String> vehicleDetails = new ArrayList<>();
+        int parkingLot = 1;
+        for (HashMap<Integer, Vehicle> vehicleHashMap : parkingLotSystem.lotMaps.values()) {
+            List vehicleList = new ArrayList<String>();
+            while (vehicleHashMap.values().remove(null)) ;
+            vehicleList = vehicleHashMap.entrySet()
+                    .stream()
+                    .filter(x -> {
+                        return (colour.equals(x.getValue().colour) && carCompany.equals(x.getValue().carCompany));
+                    })
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList());
+            for (int i = 0; i < vehicleList.size(); i++) {
+                String vehicleDetail = "PN:" + vehicleHashMap.get(vehicleList.get(i)).plateNumber + "  " +
+                        "valletName:" + vehicleHashMap.get(vehicleList.get(i)).attendantName + "  " +
+                        "L:" + parkingLot + "  " +
+                        vehicleList.get(i);
+                vehicleDetails.add(vehicleDetail);
+            }
+            parkingLot++;
+        }
+        System.out.println(vehicleDetails);
+        return vehicleDetails;
     }
 }
