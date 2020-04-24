3d0accdf791dfc50863c90d40781a535e218301f
malivaibhav15 <malivaibhav15@gmail.com>
2020-04-24 18:30:03 +0530

    ADDED : Class for poloce department

--- src/main/java/com/bridgelabz/utilities/PoliceDepartment.java
+++ src/main/java/com/bridgelabz/utilities/PoliceDepartment.java
@@ -1,4 +1,32 @@
 package com.bridgelabz.utilities;
 
-public class PoliceDepartment {
+import com.bridgelabz.service.ParkingLotSystem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class PoliceDepartment
+{
+    private final ParkingLotSystem parkingLotSystem;
+
+    public PoliceDepartment(ParkingLotSystem parkingLotSystem)
+    {
+        this.parkingLotSystem = parkingLotSystem;
+    }
+
+    public List getColouredVehicleList(String colour) {
+        List colouredVehicle = new ArrayList<String>();
+        for (HashMap<Integer, Vehicle> vehicleHashMap : parkingLotSystem.lotMaps.values()) {
+            while (vehicleHashMap.values().remove(null));
+            colouredVehicle = vehicleHashMap.entrySet()
+                    .stream()
+                    .filter(map -> colour.equals(map.getValue().colour))
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList());
+        }
+        return colouredVehicle;
+    }
 }
